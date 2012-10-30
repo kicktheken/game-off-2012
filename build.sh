b542c1811c1f7b940d68725a63c159cf90ad67b4
Kenneth Chan <kicktheken@gmail.com>
2012-10-29 22:28:12 -0700

    update build script

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 
 # script to generate optimized client build for Wirlds
 
-BUILDDIR="clientbuild"
-PROJECTDIR="client/js"
 CURDIR=$(pwd)
+DIR=`dirname ${BASH_SOURCE[0]}`
+BUILDDIR="$DIR/clientbuild"
+PROJECTDIR="$DIR/client/js"
 
 if [ "$1" == "clean" ]; then
-exit
+    set -x
+    rm -rf $BUILDDIR
+    exit
 fi
 
 echo "Building client with RequireJS"
@@ -16,7 +19,7 @@ node ../../r.js -o build.js
 cd $CURDIR
 
 echo "Moving build.txt to current dir"
-mv $BUILDDIR/build.txt $CURDIR
+mv $BUILDDIR/build.txt $DIR
 
 echo "Build complete"
 
